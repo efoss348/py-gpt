980270f9e5f0afd77e43c3fa65dc22da7fa28ff9
efoss348 <157327846+efoss348@users.noreply.github.com>
2025-01-26 18:03:59 -0600

    Update installation requirements

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-# This script is used to install the app dependencies and run the app using the virtual environment
-python -m venv ./venv
-source ./venv/bin/activate
+
+# Create and activate virtual environment
+python -m venv venv
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install build dependencies
+pip install wheel setuptools
+
+# Install requirements
 pip install -r requirements.txt
-python3 run.py "$@"
+
+# Install package in development mode
+pip install -e .
+
+echo "Installation complete! To start the application, activate the virtual environment and run: python -m pygpt_net"
